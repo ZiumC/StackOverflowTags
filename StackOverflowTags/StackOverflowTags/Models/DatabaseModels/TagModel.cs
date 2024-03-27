211506c5de0b26ccef522d3ea5deba6433a25b13
Rafał <90453529+ZiumC@users.noreply.github.com>
2024-03-27 16:06:52 +0100

    Created tag model

---
+++ StackOverflowTags/StackOverflowTags/Models/DatabaseModels/TagModel.cs
@@ -0,0 +1,11 @@
+﻿namespace StackOverflowTags.Models.DatabaseModels
+{
+    public class TagModel
+    {
+        public bool Has_synonyms { get; set; }
+        public bool Is_moderator_only { get; set; }
+        public bool Is_required { get; set; }
+        public int Count { get; set; }
+        public string? Name { get; set; }
+    }
+}
